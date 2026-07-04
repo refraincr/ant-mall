d1182cceb3c2d48e7494fbbf9ede99cdc5ab36f8
refraincr <zhongxinchun4@gmail.com>
2026-07-04 14:37:55 +0800

    创建项目

---
+++ ant-mall/src/main/java/com/example/ant_mall/AntMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.ant_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntMallApplication.class, args);
+	}
+
+}
